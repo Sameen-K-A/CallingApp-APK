3a5fc6e67da65908ad473fc5220c6380e1253b6c
Sameen-K-A <sameensameen60@gmail.com>
2025-12-12 13:16:34 +0530

    Update telecaller screen font sizing

--- app/(app)/(telecaller)/dashboard.tsx
+++ app/(app)/(telecaller)/dashboard.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function Dashboard() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-background">
-      <Text className="text-primary font-nexaHeavy text-lg">
+      <Text className="text-primary font-nexaHeavy text-lg" allowFontScaling={false}>
         Telecaller Dashboard Content will add soon
       </Text>
     </SafeAreaView>
--- app/(app)/(telecaller)/wallet.tsx
+++ app/(app)/(telecaller)/wallet.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function Wallet() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-background">
-      <Text className="text-primary font-nexaHeavy text-lg">
+      <Text className="text-primary font-nexaHeavy text-lg" allowFontScaling={false}>
         Telecaller Wallet Content will add soon
       </Text>
     </SafeAreaView>
